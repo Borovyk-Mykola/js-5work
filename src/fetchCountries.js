c3a5b168e1910b206c274a31a92482c4b68c62e9
Borovyk-Mykola <114910601+Borovyk-Mykola@users.noreply.github.com>
2023-04-05 18:29:41 +0300

    part4

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,36 +1,13 @@
 import Notiflix from 'notiflix';
 
-const refs = {
-    seachBox: document.querySelector('[id="search-box"]'),
-    countryList: document.querySelector('.country-list'),
-}
+function fetchCountries(name) {
+	const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
 
-function onSeach(e) {
-	const nameCountry = refs.seachBox.value;
-    const currentCountry = nameCountry.trim();
-	const url = `https://restcountries.com/v3.1/name/${currentCountry}?fields=name,capital,languages,population,flag`;
-
-	fetch(url).then(response => {
+	return fetch(url).then(response => {
         if(response.status === 404) {
-            return;
+            Notiflix.Notify.failure('Oops, there is no country with that name');
         }
-        else {return response.json()};
-    }).then(fetchList => {
-            if(fetchList.length > 10){
-                Notiflix.Notify.info('Too many matches found. Please entar a more specific name');
-            }
-            else if(2 <= fetchList.length <= 10){
-            for(let i = 0; i <= 10; i += 1){
-                
-                refs.countryList.textContent += 
-                fetchList[i].population 
-            }}
-            
-            else {return console.log(fetchList[0].population)}
-        }    
-    ).catch(error => {Notiflix.Notify.failure('Oops, there is no country with that name')
-    });
-    
+        return response.json();
+    })
 }
-
-export {onSeach, refs}
+export {fetchCountries}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,85 @@
 import './css/styles.css';
-import {onSeach, refs} from "./fetchCountries"
-
+import {fetchCountries} from "./fetchCountries";
+import Notiflix from 'notiflix';
 var debounce = require('lodash.debounce');
-const d = debounce(onSeach, 300)
+
+
 const refs = {
+    searchBox: document.querySelector('[id="search-box"]'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info')
+};
 
-countryInfo: document.querySelector('.country-info')
+const debounceFetchCountries = debounce(onSearch, 300);
+refs.searchBox.addEventListener('input', debounceFetchCountries);
+
+function onSearch(e) {
+    e.preventDefault();
+    const value = refs.searchBox.value.trim();
+    console.log(value);
+
+    if (!value) {
+        refs.countryList.style.visibility = 'hidden';
+        refs.countryInfo.style.visibility = 'hidden';
+        refs.countryList.innerHTML = '';
+        refs.countryInfo.innerHTML = '';
+        return;
+    }
+  
+    fetchCountries(value)
+    .then(countriesList => {
+        if (countriesList.length > 10) {
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        };
+        renderCountries(countriesList);
+    })
+    .catch(err => {
+        refs.countryList.innerHTML = '';
+        refs.countryInfo.innerHTML = '';
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
 }
-
-refs.seachBox.addEventListener('input', d)
-
+  
+function generateCountryInfo(data) {
+    data.reduce(
+      (acc, { flags: { svg }, name, capital, population, languages }) => {
+        console.log(languages);
+        languages = Object.values(languages).join(', ');
+        console.log(name);
+        return (
+          acc +
+          ` <img src="${svg}" alt="${name}" width="320" height="auto">
+              <p> ${name.official}</p>
+              <p>Capital: <span> ${capital}</span></p>
+              <p>Population: <span> ${population}</span></p>
+              <p>Languages: <span> ${languages}</span></p>`
+        );
+    },  '');
+};
+  
+function generateCountryList(data) {
+    data.reduce((acc, { name: { official, common }, flags: { svg } }) => {
+      return (
+        acc +
+        `<li>
+          <img src="${svg}" alt="${common}" width="70">
+          <span>${official}</span>
+        </li>`
+      );
+    }, '');
+};
+  
+function renderCountries(result) {
+    if (result.length === 1) {
+        refs.countryList.innerHTML = '';
+        refs.countryList.style.visibility = 'hidden';
+        refs.countryInfo.style.visibility = 'visible';
+        refs.countryInfo.innerHTML = generateCountryInfo(result);
+    }
+    if (2 <= result.length <= 10) {
+        refs.countryInfo.innerHTML = '';
+        refs.countryInfo.style.visibility = 'hidden';
+        refs.countryList.style.visibility = 'visible';
+        refs.countryList.innerHTML = generateCountryList(result);
+    }
+}
